481b9e9e44399c9ce8537f46871139d4cc15bedd
Maciej Mikolaj Faron <i291061@lab110-02.stud.ii>
2016-11-16 12:18:22 +0100

    [After lab 5]

--- pr05/ex3.ml
+++ pr05/ex3.ml
@@ -1,14 +1,28 @@
+(* 1 *)
 type htree = Node of (float * htree * htree) | Leaf of (char * float)
 
-let findLeast2 ls =
-  let find_aux lst a b newl =
-    let aux item a b newl =
-      match (item<a, item<b) with
-        (True, True) -> find_aux b item
-      | (True, False) ->
-      | (False, _) ->
-    in
-    match lst with
-      Leaf(_, prob) :: tl ->
-    | Node(prob, _, _) :: tl ->
-    | [] ->
+
+(* 2 *)
+let p = function
+    Leaf(_,prob) -> prob
+  | Node(prob,_,_) -> prob
+  ;; 
+  
+let find2 lst = function
+    [] -> lst
+  | [last] -> 
+  | a::b::tl -> find2 
+
+let rec find_l lst le nlst = 
+   match lst with
+     h::tl -> if p h < p le then find_l tl h (le::nlst) else find_l tl le (h::nlst)
+   | [] -> (le, nlst)
+
+let find3 (h::tl) = find_l tl h []
+
+let rec mkHTree lst = 
+  match lst with
+   [elem] -> elem
+ | _ -> let (smls1, rest) = find3 lst
+        in let (smls2, rest2) = find3 rest
+        in mkHTree (Node (p smls1 +. p smls2, smls1, smls2) :: rest2)
